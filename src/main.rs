7eb1534f924698a4d90d30e58ff13290a14026ba
Hand of Cthulhu <3rdhandofcthulhu@gmail.com>
2018-05-25 18:29:20 +1200

    Partial regex find

--- src/main.rs
+++ src/main.rs
@@ -55,7 +55,7 @@ fn main() {
     let mut status = String::new();
     let mut input = String::new();
 
-    //let cmd_match = regex::Regex::new(r"([a-zA-Z]+) (\d+)?").unwrap();
+    let cmd_match = regex::Regex::new(r#"(?:(next))|(?:(prev))|(?:(go) (\d+))|(?:(edit) ([0-5]) (.+))"#).unwrap();
 
     loop {
         print!("{}[2J", 27 as char); // Clear terminal
@@ -108,44 +108,69 @@ fn main() {
 //            status = "Unknown Command".to_owned();
 //        }
         //input = input.to_lowercase();
-        let mut tokens = input.split(' ');
-        let cmd = tokens.next();
-        let param = tokens.next();
-        let mut param2: String = tokens.fold("".to_owned(),|acc, c| acc + c + " ");
-        param2.pop();
-        match mode {
-            0 => match cmd {
-                Some("next") | Some("Next") => current_question_number += 1,
-                Some("prev") | Some("Prev") => current_question_number -= 1,
-                Some("go") | Some("Go") => if let Some(param) = param {
-                    if let Ok(param) = param.parse() {
-                        current_question_number = param;
-                    } else {
-                        status = "Go with invalid parameter".to_owned();
-                    }
-                } else {
-                    status = "Go with no parameter".to_owned();
-                },
-                Some("edit") | Some("Edit") => if let Some(param) = param {
-                    if let Ok(param) = param.parse::<usize>() {
-                        if param < 5 {
-                            if !param2.is_empty() {
-                                data[current_question_number as usize][param] = param2.to_owned();
-                            } else {
-                                status = "Edit with invalid parameters".to_owned();
-                            }
-                        } else {
-                            status = "Edit with invalid parameters".to_owned();
-                        }
-                    } else {
-                        status = "Edit with invalid parameter".to_owned();
-                    }
-                } else {
-                    status = "Edit with no parameters".to_owned();
-                },
-                _ => status = "Unknown Command".to_owned()
+
+        let captures = cmd_match.captures(&input).map(|captures| {
+            captures
+                .iter() // All the captured groups
+                .skip(1) // Skipping the complete match
+                .flat_map(|c| c) // Ignoring all empty optional matches
+                .map(|c| c.as_str()) // Grab the original strings
+                .collect::<Vec<_>>() // Create a vector
+        });
+
+        match captures.as_ref().map(|c| c.as_slice()) {
+            Some(["next"]) => current_question_number += 1,
+            Some(["prev"]) => current_question_number -= 1,
+            Some(["go", x]) => {
+                let x = x.parse().unwrap();
+                current_question_number = x;
+            },
+            Some(["edit", x, text]) => {
+                let x = x.parse::<usize>().unwrap();
+
+                data[current_question_number as usize][x] = text.to_owned().to_owned();
             }
-            _ => ()
+            _ => status = "Unknown command".to_owned(),
         }
+
+//        let mut tokens = input.split(' ');
+//        let cmd = tokens.next();
+//        let param = tokens.next();
+//        let mut param2: String = tokens.fold("".to_owned(),|acc, c| acc + c + " ");
+//        param2.pop();
+//        match mode {
+//            0 => match cmd {
+//                Some("next") | Some("Next") => current_question_number += 1,
+//                Some("prev") | Some("Prev") => current_question_number -= 1,
+//                Some("go") | Some("Go") => if let Some(param) = param {
+//                    if let Ok(param) = param.parse() {
+//                        current_question_number = param;
+//                    } else {
+//                        status = "Go with invalid parameter".to_owned();
+//                    }
+//                } else {
+//                    status = "Go with no parameter".to_owned();
+//                },
+//                Some("edit") | Some("Edit") => if let Some(param) = param {
+//                    if let Ok(param) = param.parse::<usize>() {
+//                        if param < 5 {
+//                            if !param2.is_empty() {
+//                                data[current_question_number as usize][param] = param2.to_owned();
+//                            } else {
+//                                status = "Edit with invalid parameters".to_owned();
+//                            }
+//                        } else {
+//                            status = "Edit with invalid parameters".to_owned();
+//                        }
+//                    } else {
+//                        status = "Edit with invalid parameter".to_owned();
+//                    }
+//                } else {
+//                    status = "Edit with no parameters".to_owned();
+//                },
+//                _ => status = "Unknown Command".to_owned()
+//            }
+//            _ => ()
+//        }
     }
 }
